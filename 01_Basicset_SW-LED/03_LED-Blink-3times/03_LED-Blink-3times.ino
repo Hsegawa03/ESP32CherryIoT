ef43f3d3b35b102dd7ed36d6d3636de3896004b8
Hasegawa03 <hasegawa2lvr@gmail.com>
2024-07-29 12:22:42 +0900

    first commit

--- 01_Basicset_SW-LED/03_LED-Blink-3times/03_LED-Blink-3times.ino
+++ 01_Basicset_SW-LED/03_LED-Blink-3times/03_LED-Blink-3times.ino
@@ -1,16 +1,26 @@
-const int ledPin = 4;
+const int ledPin = 25;
 //4:ConnectorA 9:ConnectorB 10:Builtin
 
 void setup() {
   pinMode(ledPin, OUTPUT);
 }
 
+// void loop() {
+//   for(int i = 0; i < 5; i++){
+//     digitalWrite(ledPin, HIGH); //ON
+//     delay(500); //0.5sec
+//     digitalWrite(ledPin, LOW); //OFF
+//     delay(500); //0.5sec
+//   }
+//   delay(2000); //2sec
+// }
+
 void loop() {
-  for(int i = 0; i < 3; i++){
+  for(int i = 0; i < 5; i++){
     digitalWrite(ledPin, HIGH); //ON
-    delay(500); //0.5msec
+    delay(50); //0.5sec
     digitalWrite(ledPin, LOW); //OFF
-    delay(500); //0.5msec
+    delay(50); //0.5sec
   }
-  delay(2000); //2sec
+  delay(1000); //2sec
 }
